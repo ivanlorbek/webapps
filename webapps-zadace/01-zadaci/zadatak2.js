aa457f931c24a2bfbbf585f99c6ea0e5f380891a
DrGilderoy <ilorbek@student.unipu.hr>
2022-10-10 16:26:04 +0200

    rijesen zadatak 2

---
+++ webapps-zadace/01-zadaci/zadatak2.js
@@ -0,0 +1,13 @@
+//funkcija za provjeru broja unutar raspona
+
+const br = 6578;
+
+const low = 100;
+const high = 150000;
+
+if (br > low && br < high) {
+  console.log('Broj je unutar raspona');
+} 
+else {
+  console.log('Broj nije unutar raspona');
+}
